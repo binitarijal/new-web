8cce5ae5214960b674f32fa0afc3941571c3976b
Binita rijal <rijalbinita01@gmail.com>
2025-01-25 22:19:01 +0545

    new one

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-console.log("hello world!!")
-console.log("hehhe");
-var a='jffkgk';
-var b='kndfnzd0';
+// console.log("hello world!!")
+// console.log("hehhe");
+// console.log("uffs");
+// console.log("lolk");
+// var a='jffkgk';
+// var b='kndfnzd0';
+// for(var i=0;i<=10;i++){
+//     console.log(i,"bini");
+// }
+var i=0;
+while(i<10){
+    console.log("heheh");
+}
